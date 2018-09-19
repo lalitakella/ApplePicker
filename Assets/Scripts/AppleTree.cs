a1c6a257e3679e7f1290b151262f3c09038bd1e7
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-09-19 15:45:19 -0400

    appltree script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
-    [Header("Set in Inspectpr")]
+    [Header("Set in Inspector")]
     public GameObject applePrefab;
 
     public float speed = 1f;
@@ -14,7 +14,32 @@ public class AppleTree : MonoBehaviour {
 
     public float secondsBetweenAppleDrops = 1f;
 
-    void Start () {}
-	
-	void Update () {}
+    void Start () {
+        Invoke("DropApple", 2f);
+    }
+	void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    void FixedUpdate()
+    {
+         if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
 }
