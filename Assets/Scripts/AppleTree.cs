264562ec780dcfbdbec5ddf83c36c9681527c7f0
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-09-19 14:54:15 -0400

    first script done

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspectpr")]
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start () {}
+	
+	void Update () {}
+}
